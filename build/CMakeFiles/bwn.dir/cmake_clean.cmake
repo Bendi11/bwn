15133f8d7bc9a8ddcaeae740eb04017eba483be4
Bendi11 <bkliebmann@gmail.com>
2025-08-19 20:43:04 -0700

    CMake project setup

---
+++ build/CMakeFiles/bwn.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bwn.dir/link.d"
+  "CMakeFiles/bwn.dir/src/main.cpp.o"
+  "CMakeFiles/bwn.dir/src/main.cpp.o.d"
+  "bwn"
+  "bwn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bwn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/bwn.dir/src/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/bwn.dir/src/main.cpp.o: /home/benl/dev/minis/bwn/src/main.cpp \
+ /usr/include/stdc-predef.h
